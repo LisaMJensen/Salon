c314975a64b6b690917bbdaa438ad683f0697536
Lisa Jensen <leese_38@yahoo.com>
2019-08-21 16:00:59 -0700

    fix stylist details not displaying client list by adding viewbag to details view

--- HairSalon/Models/SalonCatalogContext.cs
+++ HairSalon/Models/SalonCatalogContext.cs
@@ -4,9 +4,10 @@ namespace SalonCatalog.Models
 {
   public class SalonCatalogContext : DbContext
   {
-    public DbSet<Client> Clients { get; set; }
     public virtual DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
     
+  
     public SalonCatalogContext(DbContextOptions options) : base(options) { }
   }
 }
